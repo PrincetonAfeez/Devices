d3849272e3f4ce469b4d4a124cf0fd7cf81ca95f
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-12 17:01:32 -0700

    DevicePanel to register devices, list, lookup by ID, and report the status of all devices

--- panel.py
+++ panel.py
@@ -6,3 +6,6 @@ from devices import AlarmSystem, Camera, Device, Lock, Thermostat
 DEMO_LOCK_KEYCODE = "2468"
 DEMO_ALARM_RESET_CODE = "9999"
 
+class DevicePanel:
+    def __init__(self) -> None:
+        self._devices: dict[str, Device] = {}
